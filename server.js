345dc48b9abfb99115735614b229d0d74ab4ed19
kuldeep <mail.kuldeepvish@gmail.com>
2024-10-29 23:25:19 +0530

    New Commit

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static("public"));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, authorization");
+  next();
+});
+
 // app.use(cors({
 //   origin: "*",
 //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -39,6 +46,7 @@ const CLIENT_URL =
     ? process.env.CLIENT_URL_PROD
     : process.env.CLIENT_URL_DEV;
 
+  
 app.use(
   cors({
     origin: ["https://carrental-server-wgny.onrender.com/","http://localhost:3000","https://carrental-server-wgny.onrender.com/","http://localhost:5173/","http://localhost:5174","http://localhost:4173/"],
